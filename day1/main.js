6c2bf326907f9210f03fea4632c77b98991de179
abergwall <antonbergwall@gmail.com>
2022-12-06 21:33:22 +0100

    first day: part two

--- day1/main.js
+++ day1/main.js
@@ -1,14 +1,20 @@
-const { readFileSync } = require("fs");
+/* 
+Day 1 of adventofcode
+https://adventofcode.com/2022/day/1
+*/
 
-function main(filename) {
-  let mostCalories = 0;
+const { readFileSync } = require("fs");
+const FILENAME = "./data.txt";
+
+const readFile = () => {
+  const content = readFileSync(FILENAME, "utf-8");
+  return content.split(/\r?\n/);
+};
+
+const sumCaloriesPerElf = (data) => {
   const caloriesPerElf = [];
   let elfValues = [];
-
-  const data = readFileSync(filename, "utf-8");
-  const calories = data.split(/\r?\n/);
-
-  calories.forEach((cal) => {
+  data.forEach((cal) => {
     if (cal.length > 0) {
       elfValues.push(parseInt(cal));
     } else {
@@ -16,13 +22,30 @@ function main(filename) {
       elfValues = [];
     }
   });
-  caloriesPerElf.forEach((elf) => {
-    const sumCalories = elf.reduce((a, b) => a + b);
-    if (mostCalories < sumCalories) {
-      mostCalories = sumCalories;
-    }
+  const sumCaloriesPerElf = caloriesPerElf.map((elf) => {
+    return elf.reduce((a, b) => a + b);
   });
-  console.log(mostCalories);
-}
 
-main("./calories.txt");
+  return sumCaloriesPerElf.sort().reverse();
+};
+
+const partOne = () => {
+  const data = readFile();
+  const caloriesPerElf = sumCaloriesPerElf(data);
+  const mostCalories = caloriesPerElf[0];
+
+  console.log(
+    `1. The elf with the most calories is carrying ${mostCalories} cal`
+  );
+};
+
+const partTwo = () => {
+  const data = readFile();
+  const caloriesPerElf = sumCaloriesPerElf(data);
+  const topThreeSum = caloriesPerElf.splice(0, 3).reduce((a, b) => a + b);
+
+  console.log(`2. The total sum of the top three elfs is ${topThreeSum} cal`);
+};
+
+partOne();
+partTwo();
